259387bacfe135f91ce8c7ed66fb57547e8ade49
LanceNoble <ltnlikespie@gmail.com>
2025-01-30 23:08:02 -0500

    remove enter function

--- index.js
+++ index.js
@@ -1,36 +1,65 @@
 import { request } from "node:https";
+import { WebSocket } from "node:http";
+import { readFileSync } from "node:fs";
 
-function enter(token) {
-    let gate;
+const token = readFileSync("./token", {encoding: "utf8"});
+let raw = "";
+let vc = false;
 
-    const req = request({
-        host: "discord.com",
-        path: "/api/v10/gateway/bot",
-        protocol: "https:",
-        headers: {
-            authorization: `Bot ${token}`
-        }
-    }, (res) => {
-        res.setEncoding("utf8");
-        res.on("data", (chunk) => {
-            gate = chunk;
-        });
-    });
-
-    req.on("error", (e) => {
-        console.error(`problem with request: ${e.message}`);
-    });
-
-    req.end();
-
-    return new Promise((resolve) => {
-        const ticker = setInterval(() => {
-            if (typeof gate == "string") {
-                resolve(JSON.parse(gate)["url"]);
-                clearInterval(ticker);
+request({
+    host: "discord.com",
+    path: "/api/v10/gateway/bot",
+    protocol: "https:",
+    headers: {
+        authorization: `Bot ${token}`
+    }
+}, (res) => {
+    res.on("data", (chunk) => raw += chunk);
+    res.on("end", () => {
+        const ws = new WebSocket(`${JSON.parse(raw)["url"]}/?v=10&encoding=json`);
+        ws.onmessage = (msg) => {
+            const json = JSON.parse(msg.data);
+            //console.log(json);
+            
+            if (json["op"] == 10) {
+                setTimeout(() => {
+                    ws.send(JSON.stringify({
+                        op: 1,
+                        d: json["s"]
+                    }));
+                    ws.send(JSON.stringify({
+                        op: 2,
+                        d: {
+                            token: token,
+                            intents: 1 << 7 | 1 << 9 | 1 << 15,
+                            properties: {
+                                os: "windows",
+                                browser: "nujabes",
+                                device: "nujabes"
+                            }
+                        }
+                    }));
+                    setInterval(() => {
+                        ws.send(JSON.stringify({
+                            op: 1,
+                            d: json["s"]
+                        }))
+                    }, json["d"]["heartbeat_interval"]);
+                }, json["d"]["heartbeat_interval"] * 0/*Math.random()*/);
             }
-        })
-    });
-}
+            else if (json["op"] == 1) {
+                ws.send(JSON.stringify({
+                    op: 1,
+                    d: json["s"]
+                }));
+            }
 
-export { enter };
+            if (json["t"] == "MESSAGE_CREATE" && json["d"]["content"].charAt(0) == '~' && !vc) {
+                console.log("someone wants to play something");
+            }
+            else if (json["t"] == "MESSAGE_CREATE" && json["d"]["content"].charAt(0) == '~' && vc) {
+                
+            }
+        }
+    });
+}).end();
