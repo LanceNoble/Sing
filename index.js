5d6e8b1c1fe5686c9473989e92b866add6a05671
LanceNoble <ltnlikespie@gmail.com>
2025-02-02 23:02:26 -0500

    add concept of stream

--- index.js
+++ index.js
@@ -1,56 +1,68 @@
 import { request } from "node:https";
 import { WebSocket } from "node:http";
-import { readFileSync } from "node:fs";
+import { constants } from "node:fs";
 import { exec } from "node:child_process";
+import { open } from "node:fs/promises";
+import { exit } from "node:process";
 
 const MAX_PAGE_SIZE = 27 + (255 * 255);
 
 const pages = [];
 
-let interval = null;
-let level = 1;
 let raw = Buffer.alloc(0);
+let complete = Buffer.alloc(0);
 let i = 0;
-let complete;
+let song = 0;
 
-const exe = exec(`.\\bin\\yt-dlp.exe https://www.youtube.com/watch?v=KIXP--0-Tac -x -o -`, {
+let fileHandle;
+try {
+    fileHandle = await open(`./songs/song${song}.opus`, constants.O_CREAT | constants.O_RDONLY);
+} catch (e) {
+    console.error(e);
+    exit();
+}
+
+const interval = setInterval(async () => {
+    console.log(await fileHandle.read());
+}, 1000);
+
+exec(`.\\bin\\yt-dlp.exe -x -o "./songs/song${song++}" KIXP--0-Tac`, {
     maxBuffer: 2 ** 32,
     encoding: "buffer"
 }, (error, stdout, stderr) => {
-    // console.log(error);
-    // console.log(stderr);
-    console.log(stdout.length);
-    console.log(raw.length);
-    console.log(pages.length);
+    
 });
 
-exe.stdout.on("data", (chunk) => {
-    raw = Buffer.concat([raw, chunk]);
-    console.log(`raw length: ${raw.length}`)
-    console.log(`level: ${MAX_PAGE_SIZE * level}`)
-    if (raw.length >= MAX_PAGE_SIZE * level) {
-        while (i < raw.length) {
-            pages.push(i);
-            let pageSize = 27 + raw[i + 26];
-            for (let j = i + 27; j < i + raw[i + 26]; j++) {
-                pageSize += raw[j];
-            }
-            i += pageSize;
-        }
-        level++;
-    }
-});
 
-while (!exe.stdout.closed) {
+
+
+//const 
+
+// exe.stdout.on("data", (chunk) => {
+//     //console.log(chunk)
+//     //raw = Buffer.concat([raw, chunk]);
+//     // complete = Buffer.concat([complete, chunk]);
+//     // if (complete.length >= MAX_PAGE_SIZE) {
+//     //     raw = Buffer.concat([raw, complete]);
+//     //     complete = Buffer.alloc(0);
+//     // }
+    
+// });
+
+/*
+const interval = setInterval(() => {
     if (i < raw.length) {
-        pages.push(i);
+        //pages.push(i);
         let pageSize = 27 + raw[i + 26];
         for (let j = i + 27; j < i + raw[i + 26]; j++) {
             pageSize += raw[j];
         }
+        //pages.push(Buffer.from(raw.buffer, i, pageSize));
         i += pageSize;
+        //console.log(pages[pages.length - 1]);
     }
-}
+});
+*/
 
 // const token = readFileSync("./token", {encoding: "utf8"});
 // let raw = "";
