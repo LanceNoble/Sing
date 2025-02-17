99769252b0aaa89441d10ecec5f72c55fbe46f0e
LanceNoble <ltnlikespie@gmail.com>
2025-02-16 22:57:22 -0500

    switch from request to get

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import { request } from "node:https";
+import { get } from "node:https";
 import { WebSocket } from "node:http";
 import { constants, readFileSync } from "node:fs";
 import { exec } from "node:child_process";
 import { open, rm } from "node:fs/promises";
-import { exit } from "node:process";
 
 async function pipe(code, exe, out) {
     const pages = [];
@@ -53,57 +52,56 @@ async function pipe(code, exe, out) {
     });
 }
 
+
 const token = readFileSync("./token", { encoding: "utf8" });
-request({ host: "discord.com", path: "/api/v10/gateway/bot", protocol: "https:", headers: { authorization: `Bot ${token}` } }, (res) => {
+get("https://discord.com/api/v10/gateway/bot", { headers: { authorization: `Bot ${token}` } }, (res) => {
     let raw = "";
     res.on("data", (chunk) => raw += chunk);
     res.on("end", () => {
         const ws = new WebSocket(`${JSON.parse(raw)["url"]}/?v=10&encoding=json`);
         ws.onmessage = async (msg) => {
             const json = JSON.parse(msg.data);
-            console.log(json);
             if (json["op"] == 10) {
                 setTimeout(() => {
                     ws.send(JSON.stringify({ op: 1, d: json["s"] }));
-                    ws.send(JSON.stringify({
-                        op: 2,
-                        d: {
-                            token: token,
-                            intents: 1 << 7,
-                            properties: {
-                                os: "windows",
-                                browser: "nujabes",
-                                device: "nujabes"
-                            }
-                        }
-                    }));
+                    ws.send(JSON.stringify({ op: 2, d: { token: token, intents: 1 << 7, properties: { os: "windows", browser: "nujabes", device: "nujabes" } } }));
                     setInterval(() => ws.send(JSON.stringify({ op: 1, d: json["s"] })), json["d"]["heartbeat_interval"]);
                 }, json["d"]["heartbeat_interval"] * 0/*Math.random()*/);
             }
-            else if (json["op"] == 1) {
+            if (json["op"] == 1) {
                 ws.send(JSON.stringify({ op: 1, d: json["s"] }));
             }
-
             if (json["t"] == "INTERACTION_CREATE") {
                 raw = "";
-                const r = request({ host: "discord.com", path: `/api/v10/guilds/${json["d"]["guild_id"]}/voice-states/@me`, protocol: "https:", headers: { authorization: `Bot ${token}` } }, (res) => {
+                get(`https://discord.com/api/v10/guilds/${json["d"]["guild_id"]}/voice-states/@me`, { headers: { authorization: `Bot ${token}` } }, (res) => {
                     res.on("data", (chunk) => raw += chunk);
                     res.on("end", () => {
                         const json = JSON.parse(raw);
-                    });
-                })
-                r.end();
-                //req.on("error", (err) => console.log(err))
+                        if (json["code"] == 10065) {
+                            
+                        } else {
 
-                // const link = json["d"]["data"]["options"]["value"];
-                // try {
-                //     await open("song.opus");
-                // } catch (error) {
-                //     const file = await open("song.opus", constants.O_CREAT);
-                //     await file.close();
-                //     await rm("song.opus");
-                // }
+                        }
+                    });
+                });
             }
-        }
+        };
     });
-}).end();
+})
+// request({ host: "discord.com", path: "/api/v10/gateway/bot", protocol: "https:", headers: { authorization: `Bot ${token}` } }, (res) => {
+//     res.on("end", () => {
+//         ws.onmessage = async (msg) => {
+//             if (json["t"] == "INTERACTION_CREATE") {
+//                 //req.on("error", (err) => console.log(err))
+//                 // const link = json["d"]["data"]["options"]["value"];
+//                 // try {
+//                 //     await open("song.opus");
+//                 // } catch (error) {
+//                 //     const file = await open("song.opus", constants.O_CREAT);
+//                 //     await file.close();
+//                 //     await rm("song.opus");
+//                 // }
+//             }
+//         }
+//     });
+// }).end();
